3035b497bafe401160ec012bd49a0ae4b071f48b
Kostya Yakovlev <akovlevkosta18@gmail.com>
2025-12-27 19:32:46 +0300

    fix selenium

--- src/test/java/ru/netology/cardorder/CardOrderTest.java
+++ src/test/java/ru/netology/cardorder/CardOrderTest.java
@@ -1,59 +1,61 @@
 package ru.netology.cardorder;
 
-import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Condition.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class CardOrderTest {
+class CardOrderTest {
+
+    private WebDriver driver;
+
     @BeforeEach
     void setUp() {
-        Configuration.browser = "chrome";
-        Configuration.headless = true;
-        Configuration.browserSize = "1366x768";
-        Configuration.timeout = 5000;
-        open("http://localhost:9999");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
     }
 
     @Test
-    @DisplayName("Успешная отправка формы с валидными данными")
-    void shouldSubmitFormSuccessfully() {
-        $("[data-test-id=name] input").setValue("Иван Иванов");
-        $("[data-test-id=phone] input").setValue("+79991234567");
-        $("[data-test-id=agreement]").click();
-        $("button").click();
+    void shouldSuccessfullySubmitFormWithValidData() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input"))
+                .sendKeys("Иван Иванов");
 
-        $("[data-test-id=order-success]")
-                .shouldBe(visible)
-                .shouldHave(text("Ваша заявка успешно отправлена"));
-    }
+        driver.findElement(By.cssSelector("[data-test-id=phone] input"))
+                .sendKeys("+79012345678");
 
-    @Test
-    @DisplayName("Ошибка при невалидном телефоне")
-    void shouldShowErrorForInvalidPhone() {
-        $("[data-test-id=name] input").setValue("Иван Иванов");
-        $("[data-test-id=phone] input").setValue("89991234567");
-        $("[data-test-id=agreement]").click();
-        $("button").click();
+        driver.findElement(By.cssSelector("[data-test-id=agreement]"))
+                .click();
 
-        $("[data-test-id=phone].input_invalid .input__sub")
-                .shouldBe(visible)
-                .shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-    }
+        driver.findElement(By.tagName("button"))
+                .click();
 
+        WebElement successMessage =
+                driver.findElement(By.cssSelector("[data-test-id=order-success]"));
 
-    @Test
-    @DisplayName("Ошибка при неотмеченном чекбоксе")
-    void shouldShowErrorForUncheckedAgreement() {
-        $("[data-test-id=name] input").setValue("Иван Иванов");
-        $("[data-test-id=phone] input").setValue("+79991234567");
-        $("button").click();
-
-        $("[data-test-id=agreement].input_invalid")
-                .shouldBe(visible);
+        assertTrue(successMessage.isDisplayed());
+        assertEquals(
+                "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",
+                successMessage.getText().trim()
+        );
     }
 }
+
 
